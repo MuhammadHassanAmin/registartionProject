adfca8ccf28ef656b4f0902fe89d060121ee21b5
Hassan Amin <hasan_amin3@yahoo.com>
2019-05-25 12:18:44 +0500

    start match activity completed

--- app/src/main/java/com/example/registartionproject/StartMatch.java
+++ app/src/main/java/com/example/registartionproject/StartMatch.java
@@ -11,22 +11,40 @@ import android.widget.TextView;
 
 public class StartMatch extends AppCompatActivity {
 
+
+    Intent intent1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_match);
         SharedPreferences pref = getSharedPreferences("user_details", MODE_PRIVATE);
-        Intent intent = getIntent();
-        String opponent_id = intent.getStringExtra("opponent_id");
+        Intent intent1 = getIntent();
+        setIntent1(intent1);
+    /*    intent.getStringExtra("opponent_uid");
+        intent.getStringExtra("opponent_email");
+        intent.getStringExtra("opponent_name");
+        intent.getStringExtra("opponent_dp");
+        intent.getStringExtra("quizTopic");*/
+
+
+
+
         TextView tvTopicHeading= (TextView) findViewById(R.id.tv_QuizTopicHeading);
-        tvTopicHeading.setText(intent.getStringExtra("quizTopic"));
+        tvTopicHeading.setText(intent1.getStringExtra("quizTopic"));
         Button btnStartMatch = findViewById(R.id.btnStartMatch);
         btnStartMatch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    Intent intent = getIntent();
+                    Intent intent = new Intent(getApplicationContext(),Match.class);
+                    intent.putExtras(getintent());
                     startActivity(intent);
             }
         });
+    }
+    private Intent  getintent(){
+        return this.intent1;
+    }
+    private void setIntent1(Intent intent1) {
+        this.intent1 = intent1;
     }
 }
